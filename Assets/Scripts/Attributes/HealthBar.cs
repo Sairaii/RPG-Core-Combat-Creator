05cb5e49b744299eed44be8480256a8511089f6b
vandaria40 <vincentkonganda@yahoo.com>
2022-05-10 23:19:12 +0800

    Lecture 170

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,9 +8,17 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
+        [SerializeField] Canvas rootCanvas = null;
 
-        void Update()
+        void Update() // check [Tipp] Use the TakeDamage event question for better alternative
         {
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0) || Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
