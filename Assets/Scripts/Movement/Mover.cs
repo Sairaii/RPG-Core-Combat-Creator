136ccfe3851afde2780acccfdf6aba81f2dafe07
Vincent <vincentkonganda@yahoo.com>
2022-05-13 21:45:06 +0800

    Lecture 184 (Skip 179-183)

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -9,6 +9,7 @@ namespace RPG.Movement
     public class Mover : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] float maxSpeed = 6f;
+        [SerializeField] float maxNavPathLength = 40f;
 
         NavMeshAgent playerNavMeshAgent;
         Animator playerAnimator;
@@ -32,6 +33,17 @@ namespace RPG.Movement
         {
             GetComponent<ActionScheduler>().StartAction(this);
             MoveTo(destination, speedFraction);
+        }
+
+        public bool CanMoveTo(Vector3 destination)
+        {
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, destination, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
+            return true;
         }
 
         public void MoveTo(Vector3 destination, float speedFraction) //hit.point is vector 3
@@ -58,6 +70,18 @@ namespace RPG.Movement
         public void SetSpeed(float speed)
         {
             playerNavMeshAgent.speed = speed;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
         }
 
         public object CaptureState()
